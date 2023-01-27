fdadb543108a666ff33dbcaf9a98483f3de00611
zj5fzq <srinivas.r.bhat@email.com>
2023-01-27 12:49:59 +0530

    Corrected few bugs and formatted

---
+++ js/edit-delete.js
@@ -0,0 +1,29 @@
+
+// Fuctions for Actions
+function showEdit() {
+    var x = document.getElementById("edit");
+    x.style.display = "block";
+}
+
+function hideEdit() {
+    var x = document.getElementById("edit");
+    x.style.display = "none";
+}
+function showDelete() {
+    var x = document.getElementById("delete");
+    x.style.display = "block";
+}
+
+function hideDelete() {
+    var x = document.getElementById("delete");
+    x.style.display = "none";
+}
+function showUpload() {
+    var x = document.getElementById("upload");
+    x.style.display = "block";
+}
+
+function hideUpload() {
+    var x = document.getElementById("upload");
+    x.style.display = "none";
+}
